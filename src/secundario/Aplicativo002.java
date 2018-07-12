f556e62be5a809793fd9ff78ad5af533ab7ee5f7
ferpavanin <ferpavanin@hotmail.com>
2018-07-11 21:06:25 -0300

    teste 3

---
+++ src/secundario/Aplicativo002.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package secundario;
+
+/**
+ *
+ * @author ferpa
+ */
+public class Aplicativo002 {
+    
+    public static void main(String[] args) {
+        double i = 13;
+        double j = 3;
+        
+        System.out.println(i%j);
+    }
+    
+}
